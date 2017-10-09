c482c44bcda80e1612677b5d89d86931936d1d2f
MrWangGang <userbean@outlook.com>
2017-10-09 13:18:30 +0800

    将框架顶层定义为ORG符合团队架构

---
+++ lambframework-core/src/main/java/org/lamb/lambframework/core/analysis/AnalysisFoundation.java
@@ -0,0 +1,10 @@
+package org.lamb.lambframework.core.analysis;
+
+/**
+ * Created by WangGang on 2017/7/10 0010.
+ * E-mail userbean@outlook.com
+ * The final interpretation of this procedure is owned by the author
+ */
+public interface AnalysisFoundation {
+    public String analysis(String result);
+}
---
+++ lambframework-core/src/main/java/org/lamb/lambframework/core/exception/BusinessException.java
@@ -0,0 +1,21 @@
+package org.lamb.lambframework.core.exception;
+
+import org.lamb.lambframework.core.enumeration.ExceptionEnum;
+import org.lamb.lambframework.core.exception.basic.GlobalBasicException;
+
+/**
+ * Created by WangGang on 2017/6/22 0022.
+ * E-mail userbean@outlook.com
+ * The final interpretation of this procedure is owned by the author
+ */
+public class BusinessException extends GlobalBasicException {
+
+    public BusinessException(ExceptionEnum error) {
+        super(error);
+    }
+
+    public BusinessException(String code,String message) {
+        super(code,message);
+    }
+
+}
